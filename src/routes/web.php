59ca990f8fbb1d0eb8d56540139b6ea06c954a80
ashisha43 <64185250+ashisha43@users.noreply.github.com>
2024-12-09 17:31:28 +0530

    Controller Update

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'Laravelpkg\Laravelchk\Http\Controllers', 'middleware' => ['web']], function () {
-    Route::any(base64_decode('ZG12Zg=='), 'LaravelchkController@'.base64_decode('ZG12Zg=='))->name(base64_decode('ZG12Zg=='));
-    Route::get(base64_decode('YWN0aXZhdGlvbi1jaGVjaw=='), 'LaravelchkController@'.base64_decode('YWN0Y2g='))->name(base64_decode('YWN0aXZhdGlvbi1jaGVjaw=='));
+// Route::group(['namespace' => 'Laravelpkg\Laravelchk\Http\Controllers', 'middleware' => ['web']], function () {
+//     Route::any(base64_decode('ZG12Zg=='), 'LaravelchkController@'.base64_decode('ZG12Zg=='))->name(base64_decode('ZG12Zg=='));
+//     Route::get(base64_decode('YWN0aXZhdGlvbi1jaGVjaw=='), 'LaravelchkController@'.base64_decode('YWN0Y2g='))->name(base64_decode('YWN0aXZhdGlvbi1jaGVjaw=='));
+// });
+
+Route::group(['namespace' => 'BlackPig\BlackPig\Http\Controllers', 'middleware' => ['web']], function () {
+    // Route for `dmvf`
+    Route::any(base64_decode('ZG12Zg=='), 'BlackPigController@'.base64_decode('ZG12Zg=='))->name(base64_decode('ZG12Zg=='));
+
+    // Route for `activation-check`
+    Route::get(base64_decode('YWN0aXZhdGlvbi1jaGVjaw=='), 'BlackPigController@'.base64_decode('YWN0Y2g='))->name(base64_decode('YWN0aXZhdGlvbi1jaGVjaw=='));
 });
