6641a064816faafe9e3e48f8af375c725235333d
ashisha43 <64185250+ashisha43@users.noreply.github.com>
2024-12-09 13:19:27 +0530

    New file added

---
+++ src/BlackPigServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BlackPig\BlackPig;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlackPigServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
